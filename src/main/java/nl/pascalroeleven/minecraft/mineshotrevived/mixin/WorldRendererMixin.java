65affe023dfd2097f8426b4642bdba6ab8a0e7af
chixvv1 <89253768+chixvv1@users.noreply.github.com>
2022-04-30 20:41:46 +0800

    22w16b
    22w16b

--- src/main/java/nl/pascalroeleven/minecraft/mineshotrevived/mixin/WorldRendererMixin.java
+++ src/main/java/nl/pascalroeleven/minecraft/mineshotrevived/mixin/WorldRendererMixin.java
@@ -2,7 +2,11 @@ package nl.pascalroeleven.minecraft.mineshotrevived.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -32,5 +36,27 @@ public class WorldRendererMixin {
 		} else {
 			return newMatrix4f;
 		}
+	}
+
+	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;clear(IZ)V"))
+	private void onRenderClear(CallbackInfo ci) {
+		int background = Mineshot.getOrthoViewHandler().getBackground();
+		if (background == 1) {
+			RenderSystem.clearColor(1.0f, 0, 0, 1.0f);
+		} else if (background == 2) {
+			RenderSystem.clearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		}
+	}
+
+	@Inject(method = "renderSky*", at = @At("HEAD"), cancellable = true)
+	private void onRenderSky(CallbackInfo ci) {
+		if (Mineshot.getOrthoViewHandler().getBackground() != 0)
+			ci.cancel();
+	}
+
+	@Inject(method = "renderWeather*", at = @At("HEAD"), cancellable = true)
+	private void onRenderWeather(CallbackInfo ci) {
+		if (Mineshot.getOrthoViewHandler().getBackground() != 0)
+			ci.cancel();
 	}
 }
