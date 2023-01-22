8b9ea6a0ab53a40650b71ae44a77580626eb82fc
Pascal Roeleven <dev@pascalroeleven.nl>
2023-01-22 17:25:05 +0100

    Combine mixins

--- src/main/java/nl/pascalroeleven/minecraft/mineshotrevived/mixin/TitleScreenMixin.java
+++ src/main/java/nl/pascalroeleven/minecraft/mineshotrevived/mixin/TitleScreenMixin.java
@@ -2,10 +2,9 @@ package nl.pascalroeleven.minecraft.mineshotrevived.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.util.math.MatrixStack;
@@ -18,20 +17,15 @@ public class TitleScreenMixin extends Screen {
 		super(title);
 	}
 
-	private int color;
-
 	@ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/TitleScreen;drawStringWithShadow(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V", ordinal = 0), index = 5)
-	private int getColor(int color) {
-		return this.color = color;
-	}
-
-	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/TitleScreen;drawStringWithShadow(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V"))
-	private void onRender(MatrixStack matrices, int mouseX, int mouseY, float delta,
-			CallbackInfo info) {
+	private int getColor(MatrixStack matrices, TextRenderer textRenderer, String text, int x, int y, int color) {
 		String newVersion = Mineshot.getUpdater().getNewVersion();
+
 		if (newVersion != null) {
 			String updateMsg = "Version '" + newVersion + "' of Mineshot Revived is available!";
-			drawStringWithShadow(matrices, this.textRenderer, updateMsg, 2, 2, color);
+			drawStringWithShadow(matrices, textRenderer, updateMsg, 2, 2, color);
 		}
+
+		return color;
 	}
 }
